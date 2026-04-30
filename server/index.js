d27055775c574757e9cc17417bd5ca8b94c707de
Derrick Widner <derrick.widner@outlook.com>
2026-04-29 20:22:51 -0500

    Setup client/server structure

---
+++ server/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Redline Labs API running");
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
